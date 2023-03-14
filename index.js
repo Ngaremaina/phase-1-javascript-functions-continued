88f7f3ae21e6b473c859aeb9576fbf8b01ee45b6
Ngaremaina <ngaremaina4@gmail.com>
2023-03-14 13:14:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(hobby = "roller-skate"){  
+    return `This Saturday, I want to ${hobby}!`;
+}
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+function wrapAdjective(adj = "*"){
+    const innerFunction = function(adjective = "special"){
+        return `You are ${adj}${adjective}${adj}!`;      
+    }
+    return innerFunction;
+}
